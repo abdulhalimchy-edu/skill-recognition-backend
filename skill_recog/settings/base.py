03cf0b8f40d183fb888b9166b3157bc2e44eb9e9
Abdul Halim Chowdhury <abdulhalimchy.edu@gmail.com>
2025-10-11 23:52:59 +0300

    Add postgres database

--- skill_recog/settings/base.py
+++ skill_recog/settings/base.py
@@ -74,16 +74,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'skill_recog.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # Auth model
 AUTH_USER_MODEL = "user.User"
 
--- skill_recog/settings/development.py
+++ skill_recog/settings/development.py
@@ -1,4 +1,21 @@
+from .base import *
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+
+
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT"),
+    }
+}
